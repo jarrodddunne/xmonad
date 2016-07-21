49fc2de2934062f1dfb345d671d89bb93f62c973
jarrod <jarrodddunne@gmail.com>
2016-07-20 22:18:54 -0400

    Changed xmonad specific hotkeys to use ctrl over shift to fix caps_lock and shift mapping problem

--- xmonad.hs
+++ xmonad.hs
@@ -27,6 +27,8 @@ import System.IO
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
+import XMonad.Layout.IndependentScreens
+
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
@@ -38,7 +40,7 @@ myFocusFollowsMouse = True
 
 -- Width of the window border in pixels.
 --
-myBorderWidth   = 2
+myBorderWidth   = 1
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
@@ -108,7 +110,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,               xK_space ), sendMessage NextLayout)
 
     --  Reset the layouts on the current workspace to default
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modm .|. controlMask, xK_space ), setLayout $ XMonad.layoutHook conf)
 
     -- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
@@ -129,10 +131,10 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,               xK_Return), windows W.swapMaster)
 
     -- Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+    , ((modm .|. controlMask, xK_j     ), windows W.swapDown  )
 
     -- Swap the focused window with the previous window
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+    , ((modm .|. controlMask, xK_k     ), windows W.swapUp    )
 
     -- Shrink the master area
     , ((modm,               xK_h     ), sendMessage Shrink)
@@ -156,7 +158,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
     -- Quit xmonad
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm .|. controlMask, xK_q     ), io (exitWith ExitSuccess))
 
     -- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
@@ -166,8 +168,8 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- BEGIN ADDITIONS
 
     -- System App Launches
-    , ((modm .|. controlMask, xK_i), spawn "chromium-browser --incognito &")
-    , ((modm .|. controlMask, xK_a), spawn "atom")
+    , ((modm .|. shiftMask, xK_i), spawn "chromium-browser --incognito &")
+    , ((modm .|. shiftMask, xK_a), spawn "atom")
 
     -- Webpage App Launches
     , ((modm .|. mod1Mask , xK_e), spawn "chromium-browser --app=https://gmail.com")
@@ -192,9 +194,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     --
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    [((m .|. modm, k), windows $ onCurrentScreen f i)
+         | (i, k) <- zip (workspaces' conf) [xK_1 .. xK_9]
+         , (f, m) <- [(W.view, 0), (W.shift, controlMask)]]
     ++
 
     --
@@ -202,8 +204,8 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+        | (key, sc) <- zip [xK_w, xK_e] [0..2]
+        , (f, m) <- [(W.view, 0), (W.shift, controlMask)]]
 
 
 ------------------------------------------------------------------------
@@ -338,7 +340,7 @@ main = do
     modMask            = myModMask,
     -- numlockMask deprecated in 0.9.1
     -- numlockMask        = myNumlockMask,
-    workspaces         = myWorkspaces,
+    workspaces         = withScreens 2 myWorkspaces,
     normalBorderColor  = myNormalBorderColor,
     focusedBorderColor = myFocusedBorderColor,
 
